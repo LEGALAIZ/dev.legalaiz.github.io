ee3b701cd62f96301ed3bfb480b1f3cb276337ec
LEGALAIZ <58962153+LEGALAIZ@users.noreply.github.com>
2024-08-30 11:51:50 +0300

    Update etor.js
    	window.lampa_settings.torrents_use = false;

--- plugins/etor.js
+++ plugins/etor.js
@@ -3,7 +3,7 @@
 
 	//Не нужно переписывать весь этот код, не делайте так!
 	//for Lampa.
-	window.lampa_settings.torrents_use = true;
+	window.lampa_settings.torrents_use = false;
 	window.lampa_settings.demo = false;
 	window.lampa_settings.read_only = false;
 
